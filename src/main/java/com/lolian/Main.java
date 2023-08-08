d875fc830087b144efc858d4e9f440a5bb1fe70e
lolian <swz124@outlook.com>
2023-08-08 20:39:28 +0800

    创建项目，此项目是由Thinking in Java书籍中案例，以及一些想法

---
+++ src/main/java/com/lolian/Main.java
@@ -0,0 +1,11 @@
+package com.lolian;
+
+/**
+ * @author shen_wzhong
+ * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
